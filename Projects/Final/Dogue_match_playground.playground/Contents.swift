a9623b7e41ab0b5e4bbbe24a4c8a6fc721dd156d
MJannetty <mlgirard12@gmail.com>
2015-06-23 20:52:33 -0400

    commit changes
    starting final app (in progress)

---
+++ Projects/Final/Dogue_match_playground.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Create the user class and its parameters
+
+class User {
+    var userName: String
+    var userGeoLocation: String
+    var userEmail: String
+    var dogBreedPreference: String?
+    var dogAgePreference: String?
+    var distanceFromDog: Int?
+    var favoriteDogs: [User?] = []
+    //var userPicture: String?
+
+    init(userName:String) {
+        self.userName = userName
+    }
+}
+
+class Dog {
+    var dogName: String
+    var breed: String
+    var dogGeoLocation: String
+    //var dogPicture:
+    
+    var favoriteDogs: [User] = []
+    
+    init(dogName:String) {
+        self.dogName = dogName
+    }
+}
