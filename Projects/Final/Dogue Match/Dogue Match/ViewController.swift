02b24c492349f381a6825e284fbf73c1c6c79cdf
MJannetty <mlgirard12@gmail.com>
2015-07-07 18:24:18 -0400

    committing final changes to project
    commit changes to project

--- Projects/Final/Dogue Match/Dogue Match/ViewController.swift
+++ Projects/Final/Dogue Match/Dogue Match/ViewController.swift
@@ -38,9 +38,9 @@ class ViewController: UIViewController {
         dispatch_after(DISPATCH_TIME_NOW, dispatch_get_main_queue()) {
             // Show an image of the dog.
             let url = NSURL(string: self.currentDog!.pictureUrl!)!
-            //let dogDescript = String /("Name" + "Breed")
+//            let dogDescript: String =  "\(Name) + "Breed")"
             self.getDogImage.setImageWithURL(url)
-            self.dataTextView.text = self.currentDog!.name
+            self.dataTextView.text = "\(self.currentDog!.name), \(self.currentDog!.breed)"
             //self.dataTextView.text = self.currentDog!.breed
         }
     }
